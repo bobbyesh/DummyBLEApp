dc1196d7919180b70ec4b7fbd70078ffc30bb7a6
Eshleman, Bobby <bobby.eshleman@leviton.com>
2018-04-27 14:57:14 -0700

    Implemented scanning

--- app/src/main/java/com/example/bek/dummybleapp/MainActivity.java
+++ app/src/main/java/com/example/bek/dummybleapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bek.dummybleapp;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
@@ -7,11 +8,15 @@ import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanRecord;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,6 +42,18 @@ public class MainActivity extends AppCompatActivity {
         scannedDevices = new ArrayList<>();
         bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
 
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this, "The permission to get BLE location data is required", Toast.LENGTH_SHORT).show();
+            }else{
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }else{
+            Toast.makeText(this, "Location permissions already granted", Toast.LENGTH_SHORT).show();
+        }
+
+
         Button scanButton = findViewById(R.id.scan_button);
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,17 +63,24 @@ public class MainActivity extends AppCompatActivity {
                     public void onScanResult(int callbackType, ScanResult result) {
                         super.onScanResult(callbackType, result);
                         ScanRecord scanRecord = result.getScanRecord();
+                        Log.d(TAG, "onScanResult");
 
                         if (scanRecord != null) {
                             byte[] bytes = scanRecord.getBytes();
                             Log.d(TAG, Arrays.toString(bytes));
                         }
                     }
+
+                    @Override
+                    public void onScanFailed(int errorCode) {
+                        super.onScanFailed(errorCode);
+                        Log.d(TAG, "Scan failed (startScan): error " + errorCode);
+                    }
                 });
             }
         });
 
-        Button stopScanButton = findViewById(R.id.scan_button);
+        Button stopScanButton = findViewById(R.id.stop_scan_button);
         stopScanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,6 +89,12 @@ public class MainActivity extends AppCompatActivity {
                     public void onScanResult(int callbackType, ScanResult result) {
                         super.onScanResult(callbackType, result);
                         Log.d(TAG, "stopped scanning");
+                    }
+
+                    @Override
+                    public void onScanFailed(int errorCode) {
+                        super.onScanFailed(errorCode);
+                        Log.d(TAG, "Scan failed (stopScan): error " + errorCode);
                     }
                 });
             }
